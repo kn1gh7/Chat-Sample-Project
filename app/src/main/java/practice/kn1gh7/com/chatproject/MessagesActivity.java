9bc82b410a8d8a6e344a4c3e032ae2e5ac866c22
Mehul <mehulparmar459@gmail.com>
2016-09-18 15:49:37 +0530

    Snackbar added for Favorite add/remove

--- app/src/main/java/practice/kn1gh7/com/chatproject/MessagesActivity.java
+++ app/src/main/java/practice/kn1gh7/com/chatproject/MessagesActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -95,6 +96,11 @@ public class MessagesActivity extends AppCompatActivity {
             super.onPostExecute(successfulUpdate);
             logHelper.showLog("msgId: " + msgId + " newFavStatus: " + newFavStatus + " successStatus: " + successfulUpdate);
             msgsAdapter.notifyItemChanged(msgsAdapter.getItemPosition(msgId));
+            if (newFavStatus) {
+                showSnackbar(getString(R.string.fav_added));
+            } else {
+                showSnackbar(getString(R.string.fav_removed));
+            }
         }
     }
 
@@ -105,5 +111,9 @@ public class MessagesActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showSnackbar(String msg) {
+        Snackbar.make(messagesRV, msg, Snackbar.LENGTH_SHORT).show();
     }
 }
